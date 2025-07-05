475a00bc633180341420ef5b6ece5a7e7a774198
Muhammad Kenzo <tubaguszaki838@gmail.com>
2025-07-05 20:21:41 +0700

    🚀 Update Portfolio

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,6 +1,6 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
-import Work3 from "../../assets/work3.jpg";
+import Work3 from "../../assets/work3.png";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
 
@@ -8,23 +8,23 @@ export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: "Web Design",
+    title: "Yu-Haynes Learning Platform",
     category: "web",
     link: "https://dribbble.com/shots/21213707-Yu-Haynes-E-Learning",
   },
   {
     id: 2,
     image: Work2,
-    title: "App mobile",
+    title: "Culinara Food and Beverage App",
     category: "app",
     link: "https://dribbble.com/shots/21213741-Culinara-Food-and-Beverage-app",
   },
   {
     id: 3,
     image: Work3,
-    title: "Brand Design",
-    category: "design",
-    link: "",
+    title: "WasteWise - Waste Collector Booking",
+    category: "Mobile Design",
+    link: "https://dribbble.com/shots/26229614-WasteWise-Online-Waste-Collector-Booking?utm_source=Clipboard_Shot&utm_campaign=Muhammmad_kenzo&utm_content=WasteWise%20%20-%20Online%20Waste%20Collector%20Booking&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=Muhammmad_kenzo&utm_content=WasteWise%20%20-%20Online%20Waste%20Collector%20Booking&utm_medium=Social_Share",
   },
   {
     id: 4,
