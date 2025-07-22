500f8d5c9c35aaf153daf6a1aa4c09614fd3f91d
Muhammad Kenzo <tubaguszaki838@gmail.com>
2025-07-22 17:59:34 +0700

    🚀 Update portfolio

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,58 +1,66 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.png";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work4 from "../../assets/work4-1.png";
+import Work5 from "../../assets/work5-1.png";
+import Work6 from "../../assets/work6.png";
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
     title: "Yu-Haynes Learning Platform",
-    category: "web",
+    category: "Design",
     link: "https://dribbble.com/shots/21213707-Yu-Haynes-E-Learning",
   },
   {
     id: 2,
     image: Work2,
     title: "Culinara Food and Beverage App",
-    category: "app",
+    category: "Design",
     link: "https://dribbble.com/shots/21213741-Culinara-Food-and-Beverage-app",
   },
   {
     id: 3,
     image: Work3,
     title: "WasteWise - Waste Collector Booking",
-    category: "Mobile Design",
+    category: "Design",
     link: "https://dribbble.com/shots/26229614-WasteWise-Online-Waste-Collector-Booking?utm_source=Clipboard_Shot&utm_campaign=Muhammmad_kenzo&utm_content=WasteWise%20%20-%20Online%20Waste%20Collector%20Booking&utm_medium=Social_Share&utm_source=Clipboard_Shot&utm_campaign=Muhammmad_kenzo&utm_content=WasteWise%20%20-%20Online%20Waste%20Collector%20Booking&utm_medium=Social_Share",
   },
   {
     id: 4,
     image: Work4,
-    title: "App mobile",
-    category: "app",
-    link: "",
+    title: "Nagisa Interior Design",
+    category: "Web",
+    link: "https://NagisaInterior.github.io/nagisa-interior-design/",
   },
   {
     id: 5,
     image: Work5,
-    title: "Web Design",
-    category: "web",
-    link: "",
+    title: "Open Space",
+    category: "Web",
+    link: "https://mhktzz.github.io/open-space-website/",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "Solusi Rooftop Modern",
+    category: "Web",
+    link: "https://mhktzz.github.io/solusi-rooftop-modern/",
   },
 ];
 
 export const projectsNav = [
   {
-    name: "all",
+    name: "All",
   },
   {
-    name: "web",
+    name: "Web",
   },
   {
-    name: "app",
+    name: "App",
   },
   {
-    name: "design",
+    name: "Design",
   },
 ];
--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -4,12 +4,12 @@ import { projectsData, projectsNav } from "./Data";
 import WorkItems from "./WorkItems";
 
 const Works = () => {
-  const [item, setItem] = useState({ name: "all" });
+  const [item, setItem] = useState({ name: "All" });
   const [projects, setProjects] = useState([]);
   const [active, setActive] = useState(0);
 
   useEffect(() => {
-    if (item.name === "all") {
+    if (item.name === "All") {
       setProjects(projectsData);
     } else {
       const newProjects = projectsData.filter((project) => {
